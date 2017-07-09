d4fa6a406133c0da9eb63dc6154cdaaf3092a0cc
Omkar Poudel <opoudel@me.com>
2017-07-09 14:26:03 -0700

    Test8

--- app.py
+++ app.py
@@ -34,7 +34,11 @@ def webhook():
             for messaging_event in entry["messaging"]:
 
                 if messaging_event.get("message"):  # someone sent us a message
-                    pass
+                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                    message_text = messaging_event["message"]["text"]  # the message's text
+
+                    send_message(sender_id, "Welcome to Body & Face Clinic {{user_first_name}}!")                    
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
 
@@ -42,11 +46,7 @@ def webhook():
                     pass
 
                 if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
-                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
-
-                    send_message(sender_id, "Welcome to Body & Face Clinic {{user_first_name}}!")                    
+                    pass
 
     return "ok", 200
 
