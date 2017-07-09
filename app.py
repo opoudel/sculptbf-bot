67bf037d3253fdbbac0fc48bf4ab07fb793954bd
Omkar Poudel <opoudel@me.com>
2017-07-09 14:13:05 -0700

    Test4

--- app.py
+++ app.py
@@ -53,24 +53,6 @@ def webhook():
     return "ok", 200
 
 
-def welcome_message():
-
-    params = {
-        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
-    data = json.dumps({
-        "get_started" : {
-            "payload":"GET_STARTED_PAYLOAD"
-        }
-    })
-    r = requests.post("https://graph.facebook.com/v2.9/me/messages", params=params, headers=headers, data=data)
-    if r.status_code != 200:
-        log(r.status_code)
-        log(r.text)
-
 def send_message(recipient_id, message_text):
 
     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
