2d8f34b6eed62cb72abd4b187ddd846e32e62782
Omkar Poudel <opoudel@me.com>
2017-07-10 15:14:55 -0700

    Fixing....3

--- payloads/aboutus.py
+++ payloads/aboutus.py
@@ -13,9 +13,15 @@ def aboutus(recipient_id):
                 "elements":[
                   {
                     "title":"Welcome to Body & Face Clinic",
+                    "image_url":"https://cdn.pixabay.com/photo/2013/07/13/12/50/beauty-160456_1280.png"
                     "subtitle":"We are located at: Unit D,248 Marua Road, Mount Wellington,1051 Auckland",
-                    "url":"http://www.sculptbf.co.nz"
-                  },
+                    "default_action": {
+                      "type":"web_url",
+                      "url":"http://sculptbf.co.nz/index.php/our-prices-offers/",
+                      "messenger_extensions": true,
+                      "webview_height_ratio": "tall",
+                      "fallback_url": "http://www.sculptbf.co.nz"
+                    },
                   "buttons":[
                     {
                       "type":"phone_number",
@@ -23,6 +29,7 @@ def aboutus(recipient_id):
                       "title": "Call"
                     }
                   ]
+                  }
                 ]
               }
             }
