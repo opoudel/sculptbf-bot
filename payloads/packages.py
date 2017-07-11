e1f613839ec1301de64394955c457baaaf50a952
Omkar Poudel <opoudel@me.com>
2017-07-10 18:52:39 -0700

    more fun

--- payloads/packages.py
+++ payloads/packages.py
@@ -14,6 +14,12 @@ def packages(recipient_id):
                         {
                             "title": "Our affordable packaged plans designed just for you...",
                             "subtitle": "Select plan that best suit your need",
+                            "image_url":"https://cdn.pixabay.com/photo/2013/07/13/12/50/beauty-160456_1280.png",
+                            "default_action": {
+                                "type": "web_url",
+                                "url": "http://sculptbf.co.nz/index.php/our-prices-offers/",
+                                "webview_height_ratio": "compact"
+                            },
                             "buttons": [
                                 {
                                     "type": "web_url",
