498c4c61881c5339b4a9fcf05192b321616f985e
Omkar Poudel <opoudel@me.com>
2017-07-10 20:42:09 -0700

    Some sort of final now....

--- payloads/__init__.py
+++ payloads/__init__.py
@@ -10,9 +10,34 @@ def message(recipient_id):
               "type": "template",
               "payload": {
                 "template_type": "generic",
-							  "get_started":{
-							    "payload":"GET_STARTED_PAYLOAD"
-							  }                
+                "text": "Welcome back to Body & Face Clinic! \nHow can I help you?\n",
+                "buttons":[
+                  {
+                    "type":"postback",
+                    "title": "About Us",
+                    "payload":"ABOUT_US_PAYLOAD"
+                  },
+                  {
+                    "type":"postback",
+                    "title": "Packages",
+                    "payload":"VIEW_PACKAGES_PAYLOAD"
+                  },
+                  {
+                    "type":"postback",
+                    "title": "Shop",
+                    "payload":"SHOP_PAYLOAD"
+                  },
+                  {
+                    "type":"postback",
+                    "title": "Clinic Special",
+                    "payload":"CLINIC_SPECIAL_PAYLOAD"
+                  },
+                  {
+                    "type":"phone_number",
+                    "payload":"+64095250911",
+                    "title": "Call Clinician"
+                  }
+                ]
               }
             }
           }
