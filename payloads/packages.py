b9bc508f7f400517b454ab9538f56b3be6d83f2f
Omkar Poudel <opoudel@me.com>
2017-07-10 17:45:03 -0700

    more fun

--- payloads/packages.py
+++ payloads/packages.py
@@ -29,6 +29,25 @@ def packages(recipient_id):
                                     "fallback_url": "http://sculptbf.co.nz"
                                 }
                             ]
+                        },
+                        {
+                            "title": "Our affordable packaged plans designed just for you...",
+                            "subtitle": "Select plan that best suit your need",
+                            "default_action": {
+                                "type": "web_url",
+                                "url": "http://sculptbf.co.nz/index.php/our-prices-offers/",
+                                "webview_height_ratio": "tall",
+                                "fallback_url": "http://sculptbf.co.nz/"
+                            },
+                            "buttons": [
+                                {
+                                    "title": "Email Now",
+                                    "type": "web_url",
+                                    "url": "http://sculptbf.co.nz/index.php/our-prices-offers/",
+                                    "webview_height_ratio": "tall",
+                                    "fallback_url": "http://sculptbf.co.nz"
+                                }
+                            ]
                         }
                     ],
                     "buttons": [
